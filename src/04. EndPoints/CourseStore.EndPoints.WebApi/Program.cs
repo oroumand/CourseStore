cc7985e61cd346eef886be32800ff496a218b22b
oroumand <ar.oroumand@gmail.com>
2024-07-02 07:04:57 +0330

    Update solution to .NET 8

--- src/04. EndPoints/CourseStore.EndPoints.WebApi/Program.cs
+++ src/04. EndPoints/CourseStore.EndPoints.WebApi/Program.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using CourseStore.Core.Domain.Contracts;
+using CourseStore.Infrastructures.DataAccess.DbContexts;
+using CourseStore.Infrastructures.DataAccess.Repositories;
+using CourseStore.Services.ApplicationServices;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace CourseStore.EndPoints.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CourseStoreContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("CourseStore")));
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<CustomerService, CustomerService>();
+builder.Services.AddScoped<CourseService, CourseService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
